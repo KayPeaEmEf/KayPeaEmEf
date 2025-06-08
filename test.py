6700894084560894932e68dee36269450ea8a122
KayPeaEmEf <kevin.fenger@gmail.com>
2025-06-08 06:00:03 +0000

    Added user input

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+print("Hello World! I have just woken up...")
+def greet(person):
+    return f'Hello, {person}...!'
+
+person = input("What is your name?: ")
+
+print(greet(person))
